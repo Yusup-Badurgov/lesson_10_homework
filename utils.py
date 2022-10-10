02a7d84e73e4d3deb697e5918665f55e5cc9648e
User <b_usup@mail.ru>
2022-10-11 01:02:01 +0600

    add comment for func in file "utils.py"

--- utils.py
+++ utils.py
@@ -2,16 +2,27 @@ import json
 
 
 def load_candidates():
+    """
+    Функция загружает данные из json файла
+    :return: возвращает список словарей
+    """
     with open('candidates.json', 'r', encoding='utf-8') as file:
         data_list = json.load(file)
     return data_list
 
 
 def get_all():
+    """
+    :return: Возвращает список словарей кондидатов
+    """
     return load_candidates()
 
 
 def get_by_pk(pk):
+    """
+    :param pk: номер кандидата
+    :return: по номеру кандидата возвращает отдельный словарь кандидата
+    """
     candidates = get_all()
 
     for candidate in candidates:
@@ -21,6 +32,11 @@ def get_by_pk(pk):
 
 
 def get_by_skill(skill_name):
+    """
+
+    :param skill_name: название навыков
+    :return: по названию навыков возвращет спсиок кандидатов имеющих их
+    """
     candidates = get_all()
     list_candidate = []
     for candidate in candidates:
